b007305d2780115415a71d3fb22d4cb80e0568d7
NicholasDevelop <marcaccini.n@gmail.com>
2022-06-30 14:17:40 +0200

    Creata classe utente

--- Utente.cs
+++ Utente.cs
@@ -8,6 +8,19 @@ namespace csharp_biblioteca
 {
     internal class Utente
     {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public long tel { get; set; }
 
+        public Utente (string name, string surname, string email, string password, long tel)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.email = email;
+            this.password = password;
+            this.tel = tel;
+        }
     }
 }
