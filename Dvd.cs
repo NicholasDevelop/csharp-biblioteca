22459d6b038470e62d98877672ac6e76520d65f4
NicholasDevelop <marcaccini.n@gmail.com>
2022-06-30 14:28:45 +0200

    Aggiunta classe per Libro e per Dvd

--- Dvd.cs
+++ Dvd.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace csharp_biblioteca
 {
-    internal class Dvd
+    internal class Dvd : Documenti
     {
+        public long serialNumber { get; set; }
+        public int time { get; set; }
+
+        public Dvd(long serialNumber, int time, string title, string date, string genre, bool disponibility, string sectorPosition, string author) : base(title, date, genre, disponibility, sectorPosition, author)
+        {
+            this.serialNumber = serialNumber;
+            this.time = time;
+        }
     }
 }
--- Libro.cs
+++ Libro.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace csharp_biblioteca
 {
-    internal class Libro
+    internal class Libro : Documenti
     {
+        public long isbn { get; set; }
+        public int nPages { get; set; }
 
+        public Libro (long isbn, int nPages, string title, string date, string genre, bool disponibility, string sectorPosition, string author) : base(title, date, genre, disponibility, sectorPosition, author)
+        {
+            this.isbn = isbn;
+            this.nPages = nPages;
+        }
     }
 }
